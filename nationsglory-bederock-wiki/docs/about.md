20addf30dd218a979e00b7f4f393a7e56fbe2cd9
Dadadou972 <167913723+Dadadou972@users.noreply.github.com>
2024-06-26 17:15:32 +0000

    df

---
+++ nationsglory-bederock-wiki/docs/about.md
@@ -0,0 +1,18 @@
+---
+sidebar_position: 1
+---
+
+# Bienvenue sur le wiki!
+
+Nations glory est un des plus gros serveurs miencraft francais, ouvert en 2013 il comptabilise plusieur centaines de milier des joueurs. Nous sommes ici pour te présenter la version bedrock de nationsglory.
+Ouverte en 2020, ces serveurs offrent une experience de jeu unique , une communauté exceptionnele est un gameplay hors du commun.
+
+Tu est nouveau? Pas de soucis prend le temps de lire ce wiki ou nous te guideront pas à pas dans ta conquête du monde.
+
+:::warning Ce wiki n'est pas officiel mais il a étédevelopper pas des joueurs qui trouvait le wiki pas asser détaillé. :::
+:::Tips si tu souhaite nous aider tu peut mp discord Theking97 ::::
+
+La bande annonce de nations glory bedrock: 
+
+
+https://www.youtube.com/watch?v=Mlhq0ItfGf8&t=1s
--- nationsglory-bederock-wiki/docusaurus.config.ts
+++ nationsglory-bederock-wiki/docusaurus.config.ts
@@ -1,66 +1,48 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import sidebars from './sidebars'; // Assurez-vous que ce chemin est correct
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
+  title: 'Nationsglory Bedrock Wiki+',
+  tagline: 'Un wiki plus avancé pour Nationsglory Bedrock',
+  favicon: 'img/ng.ico',
   url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Dadadou972',
+  projectName: 'wiki-ngbe',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: require.resolve('./sidebars'),
+          editUrl: 'https://github.com/Dadadou972/wiki-ngbe/edit/main/website/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: require.resolve('./src/css/custom.css'),
         },
-      } satisfies Preset.Options,
+      },
     ],
   ],
-
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    announcementBar: {
+      id: "prerelease",
+      content: 'Bienevnue! Ce wiki est actuellement en développement, de nombreuses choses sont en devéloppement. Envie de nous aider? Contartez nous en mp Theking97 sur',
+      backgroundColor: "#df4b30",
+      textColor: "#ffffff",
+      isCloseable: true,
+    },
     navbar: {
-      title: 'My Site',
+      title: 'Nations glory bedrock wiki+',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Un wiki plus avancé pour nationsglory bedrock',
         src: 'img/logo.svg',
       },
       items: [
@@ -68,12 +50,11 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Wiki',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          href: 'https://discord.gg/aFB3QW3Hfx',
+          label: 'discord ngbe creator',
           position: 'right',
         },
       ],
@@ -81,53 +62,15 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+        // Insérez vos liens de footer ici
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // Insérez votre notice de droit d'auteur ici
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
-  } satisfies Preset.ThemeConfig,
+  },
 };
 
 export default config;
